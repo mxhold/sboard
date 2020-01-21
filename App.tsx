8dba4aaa1761d5b21b33d421418441bfcc6d1177
Max Holder <max.holder@carbonfive.com>
2020-01-21 15:35:08 -0800

    Add airhorn button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
+import { Audio } from 'expo-av';
+
+const onPressButton = async () => {
+  const sound = new Audio.Sound();
+  try {
+    await sound.loadAsync(require('./assets/sounds/airhorn.wav'));
+    await sound.playAsync();
+  } catch (error) {
+    alert('Error playing sound: ' + error);
+  }
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button onPress={onPressButton} title="Airhorn" />
     </View>
   );
 }
