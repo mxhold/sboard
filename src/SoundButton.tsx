cec6c01d56fb0a34ee8c9cec852e50270f1f572d
Max Holder <max.holder@carbonfive.com>
2020-01-22 14:14:01 -0800

    Add beautiful button image

--- src/SoundButton.tsx
+++ src/SoundButton.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Audio } from 'expo-av';
-import { Button } from 'react-native';
+import { Image, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 import { PlaybackSource } from 'expo-av/build/AV';
 
 type Props = {
@@ -9,10 +9,12 @@ type Props = {
 }
 
 export default function SoundButton({ source, prompt}: Props) {
+  const [isDepressed, setIsDepressed] = useState(false);
+
   const onPress = async () => {
     const sound = new Audio.Sound();
     try {
-      await Audio.setAudioModeAsync({ playsInSilentModeIOS: true })
+      await Audio.setAudioModeAsync({ playsInSilentModeIOS: true });
       await sound.loadAsync(source);
       await sound.playAsync();
     } catch (error) {
@@ -20,7 +22,39 @@ export default function SoundButton({ source, prompt}: Props) {
     }
   }
 
+  let imageSource;
+  if (isDepressed) {
+    imageSource = require('../assets/button-pressed.png');
+  } else {
+    imageSource = require('../assets/button.png');
+  }
+
   return (
-    <Button onPress={onPress} title={prompt} />
+    <TouchableWithoutFeedback
+      onPress={onPress}
+      onPressIn={() => setIsDepressed(true)}
+      onPressOut={() => setIsDepressed(false)}
+    >
+      <View style={styles.container}>
+        <Image style={styles.button} source={imageSource} />
+        <Text style={styles.prompt}>{prompt}</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  prompt: {
+    position: 'absolute',
+    color: '#fff',
+    fontSize: 20,
+  },
+  button: {
+    width: 200,
+    height: 200
+  }
+});
