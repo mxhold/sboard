76a3f5111fa080c98f529745286d99353406d035
Max Holder <max.holder@carbonfive.com>
2020-01-27 09:30:09 -0800

    Extract SoundBoard, add rn-safe-area-context

--- App.tsx
+++ App.tsx
@@ -1,33 +1,11 @@
 import React from "react";
-import { Platform, StyleSheet, SafeAreaView } from "react-native";
-import SoundButton from "./src/SoundButton";
+import SoundBoard from "./src/SoundBoard";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  const sounds = [
-    { source: require("./assets/sounds/airhorn.wav"), prompt: "Airhorn" },
-    {
-      source: require("./assets/sounds/record-scratch.wav"),
-      prompt: "Record scratch"
-    },
-    { source: require("./assets/sounds/rimshot.wav"), prompt: "Rimshot" },
-    { source: require("./assets/sounds/register.wav"), prompt: "Cash Register" }
-  ];
-
   return (
-    <SafeAreaView style={styles.container}>
-      {sounds.map(({ source, prompt }) => (
-        <SoundButton key={source} source={source} prompt={prompt} />
-      ))}
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SoundBoard />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    flexDirection: Platform.OS === "web" ? "row" : "column"
-  }
-});
---
+++ src/SoundBoard.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Platform, StyleSheet } from "react-native";
+import SoundButton from "./SoundButton";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function SoundBoard() {
+  const sounds = [
+    { source: require("../assets/sounds/airhorn.wav"), prompt: "Airhorn" },
+    {
+      source: require("../assets/sounds/record-scratch.wav"),
+      prompt: "Record scratch"
+    },
+    { source: require("../assets/sounds/rimshot.wav"), prompt: "Rimshot" },
+    {
+      source: require("../assets/sounds/register.wav"),
+      prompt: "Cash Register"
+    }
+  ];
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {sounds.map(({ source, prompt }) => (
+        <SoundButton key={source} source={source} prompt={prompt} />
+      ))}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: Platform.OS === "web" ? "row" : "column"
+  }
+});
